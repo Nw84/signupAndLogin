2315c29f9bc0ef7c352197ca50c05e3fc77d749b
Nordwall <tobiasnordwall@gmail.com>
2022-03-04 22:17:40 +0100

    I made a minor change to the transition speed

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,7 @@ module.exports = {
         }
       },
       animation: {
-        "fade-in-down": "fade-in-down 2s ease-out"
+        "fade-in-down": "fade-in-down 1s ease-out"
       }
     },
   },
